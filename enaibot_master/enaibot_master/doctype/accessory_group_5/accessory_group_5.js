54fe5e8359ca0300a4e55e4d906cc293516435cd
Abhishek Bankar <abhishekraje30@gmail.com>
2024-09-13 17:52:40 +0530

    Master data raw schema added partially

---
+++ enaibot_master/enaibot_master/doctype/accessory_group_5/accessory_group_5.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Abhishek Bankar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Accessory Group 5", {
+// 	refresh(frm) {
+
+// 	},
+// });
